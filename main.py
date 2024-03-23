d8b0e09191b52653ddd8ca78b744d54177a925fc
gruand <gruand@gmail.com>
2024-03-23 19:12:28 +0200

    Prepare to convert to the package

---
+++ main.py
@@ -0,0 +1,5 @@
+import invoice
+
+invoice.generate("invoices", "new_pdfs", "logo.png",
+                 "product_id", "product_name", "amount_purchased",
+                 "price_per_unit", "total_price")
